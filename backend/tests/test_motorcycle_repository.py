ceff419e5bb55e89cd985cbd540fecd19b59655d
Juan Alvarez <juan.avallado@gmail.com>
2026-07-13 16:11:34 +0200

    added test cases

--- backend/tests/test_motorcycle_repository.py
+++ backend/tests/test_motorcycle_repository.py
@@ -17,3 +17,33 @@ def test_repository_creates_motorcycle():
     assert motorcycle.id is not None
     for field_name, expected_value in motorcycle_data.items():
         assert getattr(motorcycle, field_name) == expected_value
+
+def test_repository_gets_motorcycle_by_id():
+    repo = InMemoryMotorcycleRepository()
+
+    payload = MotorcycleCreate(make="KTM", model="Duke 390", year=2023)
+    created_motorcycle = repo.create(payload)
+    id = created_motorcycle.id
+
+    retrieved_motorcycle = repo.get(id)
+    assert retrieved_motorcycle is not None
+    assert retrieved_motorcycle.id == id
+
+def test_repository_lists_motorcycles():
+    repo = InMemoryMotorcycleRepository()
+
+    payload1 = MotorcycleCreate(make="KTM", model="Duke 390", year=2023)
+    payload2 = MotorcycleCreate(make="Yamaha", model="R1", year=2022)
+
+    repo.create(payload1)
+    repo.create(payload2)
+
+    motorcycles = repo.list()
+    expected_motorcycles = [
+        ("KTM", "Duke 390", 2023),
+        ("Yamaha", "R1", 2022),
+    ]
+
+    assert [(motorcycle.make, motorcycle.model, motorcycle.year) 
+            for motorcycle in motorcycles] == expected_motorcycles
+    
