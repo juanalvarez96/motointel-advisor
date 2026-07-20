990d009dfa444175d392f39f13edf465f29150b2
Juan Alvarez <juan.avallado@gmail.com>
2026-07-20 19:00:01 +0200

    app/api/motorcycle.py failing!!

--- backend/app/api/motorcycles.py
+++ backend/app/api/motorcycles.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from app.domain.motorcycle import Motorcycle
+from backend.app.api.dependencies import get_motorcycle_repository
+from backend.app.repositories.motorcycle_repository import InMemoryMotorcycleRepository
+from backend.app.domain.motorcycle import MotorcycleCreate
+from backend.app.services.motorcycle_service import MotorcycleService
 
 
 
@@ -8,7 +12,8 @@ router = APIRouter()
 @router.post(
         "/motorcycles",
         response_model=Motorcycle,
-        status_code=201,
+        status_code=status.HTTP_201_CREATED,
 )
-def create_motorcycle():
-    raise NotImplemented()
+def create_motorcycle(payload: MotorcycleCreate, repository: InMemoryMotorcycleRepository = Depends(get_motorcycle_repository)) -> Motorcycle:
+    service = MotorcycleService(repository)
+    return service.create_motorcycle(payload)
