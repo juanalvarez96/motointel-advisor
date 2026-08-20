127506b0896d28cd1f851691d3a7deedf4c743da
Juan Alvarez <juan.avallado@gmail.com>
2026-08-20 21:43:15 +0200

    dummy

--- backend/tests/test_motorcycle_api.py
+++ backend/tests/test_motorcycle_api.py
@@ -55,6 +55,7 @@ def test_get_motorcycle_endpoint() -> None:
     get_response = client.get(f"/motorcycles/{motorcycle_id}")
 
     assert get_response.status_code == 200
+    #dummy!!
 
     retrieved = get_response.json()
 
